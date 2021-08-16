9db88ee97ad2d4eac057f912e57666a544c047de
Shakil-Mehrab <mehrabhoussainshakil12@gmail.com>
2021-08-16 21:55:41 +0600

    commit with breadcrumb and list table

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -1,33 +1,7 @@
 <x-app-layout>
     <main class="flex-1 overflow-y-auto">
         <div class="pt-8 w-full mx-auto px-4 sm:px-6 lg:px-8">
-            {{-- bredcrumb --}}
-            <div class="flex">
-                <h1 class="flex-1 text-2xl font-bold text-gray-900">Photos</h1>
-                <div class="ml-6 bg-gray-100 p-0.5 rounded-lg flex items-center sm:hidden">
-                    <button type="button"
-                        class="p-1.5 rounded-md text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <!-- Heroicon name: solid/view-list -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                            aria-hidden="true">
-                            <path fill-rule="evenodd"
-                                d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        <span class="sr-only">Use list view</span>
-                    </button>
-                    <button type="button"
-                        class="ml-0.5 bg-white p-1.5 rounded-md shadow-sm text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <!-- Heroicon name: solid/view-grid -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                            aria-hidden="true">
-                            <path
-                                d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                        </svg>
-                        <span class="sr-only">Use grid view</span>
-                    </button>
-                </div>
-            </div>
+            <x-header.breadcrumb></x-header.breadcrumb>
 
             <div class="mt-3 sm:mt-2">
                 <div class="sm:hidden">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Media\MediaController;
+use App\Http\Controllers\Admin\Article\ArticleController;
 use App\Http\Controllers\Admin\Setting\SettingController;
 
 /*
@@ -20,3 +21,4 @@ Route::get('/', function () {
 });
 Route::get('setting', [SettingController::class, 'index']);
 Route::resource('media', MediaController::class);
+Route::resource('articles', ArticleController::class);
